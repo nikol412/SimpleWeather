18356032d21fd59b46dbfc532f5f9642e1dbf601
Nikol 412 <travin.nikita@gmail.com>
2020-03-12 00:42:22 +0600

    add text in VM

--- app/src/main/java/ru/nikol/simplyweather/OverviewViewModel.kt
+++ app/src/main/java/ru/nikol/simplyweather/OverviewViewModel.kt
@@ -4,7 +4,11 @@ import androidx.lifecycle.ViewModel
 
 class OverviewViewModel : ViewModel() {
     var city = "Омск"
-
+    var degrees = "+27"
+    var windSize = "123 м/с, эльфийский"
+    var pressureSize = "302 мм.рт.ст."
+    var humidity = "-20%"
+    var rain = "120%"
     init {
 
     }
