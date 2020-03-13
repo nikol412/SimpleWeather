829e34d5b0eb168c4b53f3ec40173f07e8ee44eb
Nikol 412 <travin.nikita@gmail.com>
2020-03-13 22:56:28 +0600

    adds some fixes for entity

--- app/src/main/java/ru/nikol/simplyweather/WeatherEntity.kt
+++ app/src/main/java/ru/nikol/simplyweather/WeatherEntity.kt
@@ -1,10 +1,12 @@
 package ru.nikol.simplyweather
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+@Entity(tableName = "weather_items")
 data class WeatherEntity(
-    @PrimaryKey var id: Int,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
     @ColumnInfo(name = "city") var city: String,
     @ColumnInfo(name = "windSpeed") var windSpeed: Double,
     @ColumnInfo(name = "windDegrees") var windDegrees: Int,
