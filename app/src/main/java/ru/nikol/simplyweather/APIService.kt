c8d66d19674d37ac4410735d656bca5a9630cad5
Nikol 412 <travin.nikita@gmail.com>
2020-03-13 12:30:19 +0600

    some changes in APIService

--- app/src/main/java/ru/nikol/simplyweather/APIService.kt
+++ app/src/main/java/ru/nikol/simplyweather/APIService.kt
@@ -4,9 +4,11 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 
-
-
 interface APIService {
-    @GET("data/2.5/weather?id=1496153&appid=ae21efbdc75d07343cb2a27262ded06e")
+//    @GET("data/2.5/weather?id=1496153&appid=ae21efbdc75d07343cb2a27262ded06e")
+//    fun loadWeather(): Call<CurrentWeather?>?
+
+    @GET("data/2.5/weather?q=Omsk,ru&appid=ae21efbdc75d07343cb2a27262ded06e")
     fun loadWeather(): Call<CurrentWeather?>?
+    //@GET("data/2.5/weather?q=Omsk&appid=ae21efbdc75d07343cb2a27262ded06e")
 }
